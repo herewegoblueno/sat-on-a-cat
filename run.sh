4655d4af6b7948b34c70108741e3a6f7d9f7a6e8
OceanPak <21999617+OceanPak@users.noreply.github.com>
2023-02-12 13:36:30 -0500

    fixing initial connection between run.sh and main.go

--- run.sh
+++ run.sh
@@ -3,16 +3,15 @@
 ########################################
 ############# CSCI 2951-O ##############
 ########################################
-# TODO: need to restore this later
-# E_BADARGS=65
-# if [ $# -ne 1 ]
-# then
-# 	echo "Usage: `basename $0` <input>"
-# 	exit $E_BADARGS
-# fi
+E_BADARGS=65
+if [ $# -ne 1 ]
+then
+	echo "Usage: `basename $0` <input>"
+	exit $E_BADARGS
+fi
 	
-# input=$1
+input=$1
 
 # Update this file with instructions on how to run your code given an input
 # java -cp src/ solver.sat.Main $input
-./solver
+./solver $input
--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	sat "sat/pkg"
 )
 
 func main() {
+	if len(os.Args) < 1 {
+		fmt.Errorf("Error: no CNF files supplied")
+	}
 	filePath := os.Args[1]
 	sat.ParseCNFFile(filePath)
 }
